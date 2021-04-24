163192eee06812ec5f32141e21ec3906bc921f35
Mohamed Mostafa <q65@msn.com>
2021-04-24 19:18:20 +0200

    add xml sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const { getAllNodes } = require("next-mdx");
+
+(async () => {
+  const posts = await getAllNodes("post");
+
+  const sitemap = `
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      <url>
+          <loc>https://www.mohamedhedeya.com/</loc>
+          <changefreq>always</changefreq>
+          <priority>1</priority>
+          <lastmod>2021-04-24T21:03:56.418Z</lastmod>
+      </url>
+  ${posts
+    .map((post) => {
+      return `
+      <url>
+        <loc>${`https://mohamedhedeya.com${post.url}`}</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+      </url>
+      `;
+    })
+    .join("")}
+    </urlset>
+    `;
+
+  fs.writeFileSync("public/sitemap.xml", sitemap);
+})();
