8a98a5746d8b9f8227b7f313199430ba4efc6866
Mohamed Mostafa <q65@msn.com>
2021-04-25 22:54:43 +0200

    update next-mdx, add gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,37 @@
-import Document, { Html, Main, NextScript, Head } from "next/document"
-import { InitializeColorMode } from "reflexjs"
+import Document, { Html, Main, NextScript, Head } from "next/document";
+import { InitializeColorMode } from "reflexjs";
+import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default class extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body>
           <InitializeColorMode />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
