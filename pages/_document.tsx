49a258deacdfa64a34827f72bff617e82c14a339
Mohamed Mostafa <q65@msn.com>
2021-05-04 02:10:52 +0200

    add google adsense code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,6 +25,12 @@ export default class extends Document {
           `,
             }}
           />
+
+          <script
+            data-ad-client="ca-pub-8492395581433688"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
         </Head>
         <body>
           <InitializeColorMode />
