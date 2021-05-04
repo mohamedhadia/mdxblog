de3c4a1b7fe7b085505abb9023b84ffdfe965dc4
Mohamed Mostafa <q65@msn.com>
2021-05-04 14:08:33 +0200

    defer the google adsense script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,7 +28,7 @@ export default class extends Document {
 
           <script
             data-ad-client="ca-pub-8492395581433688"
-            async
+            defer
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           ></script>
         </Head>
